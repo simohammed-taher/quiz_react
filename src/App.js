cef6dae5b2864e07305eac79cdccfa1ac70f40fc
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-02-14 17:03:06 +0100

    qsm react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
-
+import question from './question';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [questionIdx, setQuestionIdx] = useState(0);
+	const [score, setScore] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const selectOption = (idx) => {
+		if (currQuestion.answer === idx) {
+			setScore(score + 1);
+		}
+		const nextQ = questionIdx + 1;
+		if (nextQ < question.length) {
+			setQuestionIdx(questionIdx + 1);
+		} else {
+			setShowScore(true);
+		}
+	};
+	const reset = () => {
+		setQuestionIdx(0);
+		setScore(0);
+		setShowScore(false);
+	};
+	const currQuestion = question[questionIdx];
+	return (
+		<div className='quiz-container'>
+			{showScore ? (
+				<>
+					<h1>your score is : {score}</h1>
+					<button onClick={reset}>Restart Quiz</button>
+				</>
+			) : (
+				<div className='quiz-container__question'>
+					<p>{currQuestion.question}</p>
+					<div className='quiz-container__options'>
+						<ul className='quiz-container__ul'>
+							{currQuestion.options.map((option, i) => {
+								return (
+									<li
+										className='quiz-container__li'
+										onClick={() => selectOption(i)}
+									>
+										{option}
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/question.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        question :"chhal fchhar lyoum",
+        options:["14","15","1","ma3art"],
+        answer:1
+    },    {
+        question :"lyouma chno",
+        options:["tnin","tlat","mardi","ma3art"],
+        answer:2
+    },    {
+        question :"kayna chta",
+        options:["ah","la","wa9ila","mohal"],
+        answer:2
+    },    {
+        question :"chkon howa yassin",
+        options:["makan3arfouch","wald r4a","wld biza","ma3art"],
+        answer:1
+    }
+]
